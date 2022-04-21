ef45f67c649765d7f57187e13651792651d68eac
Андрей Гончар <101369647+andreigon@users.noreply.github.com>
2022-04-22 01:33:36 +0300

    Add files via upload
    Инпут работает через клик и Enter

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 Vue.createApp({
     data() {
         return{
-            valueinput
+            valueInput: '',
+            needDoList: []
         };
     },
     methods: {
-
+        handlyInput(event){
+            this.valueInput = event.target.value;
+        },
+        addTask () {
+            if(this.valueInput===''){return};
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput='';
+        }
     }
 }).mount('#app');
